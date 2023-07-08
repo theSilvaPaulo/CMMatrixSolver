94f53dceb7a84431f4619f9901ccca395de400b9
Paulo <paulo99.h.silva@gmail.com>
2023-07-08 17:13:13 -0300

    added blueprint

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from app.views import app_views
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+
+app.config['FLASK_DEBUG'] = True
 
 if __name__ == '__main__':
     app.run()
