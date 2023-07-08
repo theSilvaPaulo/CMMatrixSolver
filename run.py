e1a7b520fe106fd60e31d917d38f229a32b1444e
Paulo <paulo99.h.silva@gmail.com>
2023-07-08 17:45:30 -0300

    installed cors

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from app.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 
 app.config['FLASK_DEBUG'] = True
