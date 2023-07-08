1af4aaf3e17b315e59bb740b1475e691a9893805
Paulo <paulo99.h.silva@gmail.com>
2023-07-08 16:54:19 -0300

    added venv

---
+++ app/utils.py
@@ -0,0 +1,7 @@
+def invert_diagonals(matrix):
+    # Implemente a lógica para inverter as diagonais da matriz quadrada
+    pass
+
+def count_submatrix(matrix, submatrix):
+    # Implemente a lógica para contar quantas vezes a submatriz pode ser encontrada na matriz
+    pass
---
+++ app/views.py
@@ -0,0 +1,15 @@
+from flask import app, jsonify, request
+from app.utils import invert_diagonals, count_submatrix
+
+@app.route('/invert-diagonals', methods=['POST'])
+def api_invert_diagonals():
+    matrix = request.json.get('matrix')
+    inverted_matrix = invert_diagonals(matrix)
+    return jsonify({'inverted_matrix': inverted_matrix})
+
+@app.route('/count-submatrix', methods=['POST'])
+def api_count_submatrix():
+    matrix = request.json.get('matrix')
+    submatrix = request.json.get('submatrix')
+    count = count_submatrix(matrix, submatrix)
+    return jsonify({'count': count})
